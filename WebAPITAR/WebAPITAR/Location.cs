e7d7e865b4d9df4c0a6e281a1a096c807479ab28
gowthamkg11 <37201637+gowthamkg11@users.noreply.github.com>
2018-04-23 17:28:49 +0800

    SeparateProject

---
+++ WebAPITAR/WebAPITAR/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPITAR
+{
+    public class Location
+    {
+
+       
+        public string lat { get; set; }
+        public string lng { get; set; }
+
+       
+    }
+}
---
+++ WebAPITAR/WebAPITAR/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPITAR
+{
+    public class Team
+
+    {
+        public int TeamID { get; set; }
+
+        public List<Location> Location { get; set; }
+    }
+}
